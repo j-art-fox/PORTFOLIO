928f3a6a025a23077919c403ddcc956a2ef414b2
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-11-06 18:26:14 -0700

    started development of various components for the body of the portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Nav/Navbar';
+import About from './components/Pages/About';
 
 function App() {
-  return (
-    
+  
+    switch (window.location.pathname) {
+      case "/":
+        break
+      case "/about":
+        break
+      case "/contact":
+        break
+      case "/work":
+        break
+      case "/resume":
+    }
+    return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <About />
     </div>
   );
 }
