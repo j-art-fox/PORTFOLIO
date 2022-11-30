bba20995eaed9b5186fa2ec9b7a1022388f935c0
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-11-29 19:09:15 -0700

    added project

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,20 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Link  } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer class="p-4 bg-gray-200 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <Link href="/about" class="hover:underline">J. Ring-Sakabe</Link>.  All Rights Reserved.
+    <footer className="p-4 bg-gray-200 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <Link href="/about" className="hover:underline">J. Ring-Sakabe</Link>.  All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        {/* to debug: The links below aren't working*/}
         <li>
-            <Link href="/about" class="mr-4 hover:underline md:mr-6 ">About</Link>
+            <Link href="/about" className="mr-4 hover:underline md:mr-6 ">About</Link>
         </li>
         <li>
-            <Link href="/resume" class="mr-4 hover:underline md:mr-6">Work</Link>
+            <Link href="/resume" className="mr-4 hover:underline md:mr-6">Work</Link>
         </li>
         <li>
-            <Link href="/contact" class="hover:underline">Contact</Link>
+            <Link href="/contact" className="hover:underline">Contact</Link>
         </li>
     </ul>
 </footer>
--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,15 +1,15 @@
-import designIcon from "../../Images/design.png";
-import backEndIcon from "../../Images/backend.png";
-import frontEndIcon from "../../Images/frontend.png";
+import designIcon from "../../Assets/Images/design.png";
+import backEndIcon from "../../Assets/Images/backend.png";
+import frontEndIcon from "../../Assets/Images/frontend.png";
 import { useState, useEffect } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
-import pp1 from "../../Images/pp1.png";
-import pp2 from "../../Images/pp2.png";
-import pp3 from "../../Images/pp3.png";
-import pp4 from "../../Images/pp4.png";
-import pp5 from "../../Images/pp5.png";
-import pp6 from "../../Images/pp6.png";
-import pp7 from "../../Images/pp7.png";
+import pp1 from "../../Assets/Images/pp1.png";
+import pp2 from "../../Assets/Images/pp2.png";
+import pp3 from "../../Assets/Images/pp3.png";
+import pp4 from "../../Assets/Images/pp4.png";
+import pp5 from "../../Assets/Images/pp5.png";
+import pp6 from "../../Assets/Images/pp6.png";
+import pp7 from "../../Assets/Images/pp7.png";
 
 export default function About() {
   const images = [pp1, pp2, pp3, pp4, pp5, pp6, pp7];
@@ -26,34 +26,34 @@ export default function About() {
 
   return (
     <>
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-12 mx-auto">
-          <div class="">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-12 mx-auto">
+          <div className="">
             <img
-              class="w-2/5 inline"
+              className="w-2/5 inline"
               src={images[imageIdx]}
               alt="An Image"
             />
           </div>
-          <div class="bg-gray-100 shadow">
-            <h1 class="sm:text-3xl text-2xl py-2 font-medium title-font text-center text-gray-900 mb-20 transition-opacity">
+          <div className="bg-gray-100 shadow">
+            <h1 className="sm:text-3xl text-2xl py-2 font-medium title-font text-center text-gray-900 mb-20 transition-opacity">
               Hi there!
-              <br class="sm:block" />
+              <br className="sm:block" />
               The name's Jay,
-              <br class="sm:block" />
+              <br className="sm:block" />
               I'm a React Full-Stack Developer
             </h1>
           </div>
           {/* design column */}
           <div>
-            <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-              <div class="p-4 md:w-1/3 flex">
-                <div class="flex-grow pl-6">
-                  <img class="w-1/6 inline" src={designIcon} />
-                  <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
+            <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+              <div className="p-4 md:w-1/3 flex">
+                <div className="flex-grow pl-6">
+                  <img className="w-1/6 inline" src={designIcon} />
+                  <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
                     Appealing Design
                   </h2>
-                  <p class="leading-relaxed text-base">
+                  <p className="leading-relaxed text-base">
                     As a user, I know how frustrating navigating
                     around a confusing and unappealing
                     application is. As a developer, I know how to turn it into
@@ -61,7 +61,7 @@ export default function About() {
                   </p>
                   <Link
                     to="/work"
-                    class="mt-3 text-indigo-500 inline-flex items-center"
+                    className="mt-3 text-indigo-500 inline-flex items-center"
                   >
                     Learn More
                     <svg
@@ -69,8 +69,8 @@ export default function About() {
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-4 h-4 ml-2"
+                      strokeWidth="2"
+                      className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
                     >
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -78,20 +78,20 @@ export default function About() {
                   </Link>
                 </div>
               </div>
-              <div class="p-4 md:w-1/3 flex">
-                <div class="flex-grow pl-6">
-                  <img class="w-1/6 py-3 inline" src={frontEndIcon} />
-                  <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
+              <div className="p-4 md:w-1/3 flex">
+                <div className="flex-grow pl-6">
+                  <img className="w-1/6 py-3 inline" src={frontEndIcon} />
+                  <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
                     Seamless Front-end
                   </h2>
-                  <p class="leading-relaxed text-base">
+                  <p className="leading-relaxed text-base">
                     Specialized in customized development, React, JavaScript,
                     NodeJS, HTML and CSS to help you and your business push your
                     web presence and brand further into the market.
                   </p>
                   <Link
                     to="/contact"
-                    class="mt-3 text-indigo-500 inline-flex items-center"
+                    className="mt-3 text-indigo-500 inline-flex items-center"
                   >
                     Contact Me
                     <svg
@@ -99,8 +99,8 @@ export default function About() {
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-4 h-4 ml-2"
+                      strokeWidth="2"
+                      className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
                     >
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -108,18 +108,18 @@ export default function About() {
                   </Link>
                 </div>
               </div>
-              <div class="p-4 md:w-1/3 flex">
-                <div class="flex-grow pl-6">
-                  <img class="w-1/6 py-3 inline" src={backEndIcon} />
-                  <h2 class="text-gray-900 text-lg title-font font-medium mb-2">
+              <div className="p-4 md:w-1/3 flex">
+                <div className="flex-grow pl-6">
+                  <img className="w-1/6 py-3 inline" src={backEndIcon} />
+                  <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
                     Back-end Development
                   </h2>
-                  <p class="leading-relaxed text-base">
+                  <p className="leading-relaxed text-base">
                     Need to set up a database for your web application? Trained in SQL, MongoDB, Express and database design, we'll get your server-side securely set up and processing data functioning in a jiffy.
                   </p>
                   <Link
                     to="/work"
-                    class="mt-3 text-indigo-500 inline-flex items-center"
+                    className="mt-3 text-indigo-500 inline-flex items-center"
                   >
                     Learn More
                     <svg
@@ -127,8 +127,8 @@ export default function About() {
                       stroke="currentColor"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      stroke-width="2"
-                      class="w-4 h-4 ml-2"
+                      strokeWidth="2"
+                      className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
                     >
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
